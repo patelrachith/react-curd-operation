a614405e9c4b9a10e18596600126a727a40019e5
patelrachith <patelrachith@gmail.com>
2022-08-03 11:14:55 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './style.css';
+import Test from './Components/Test';
 
 export default function App() {
   return (
     <div>
       <h1>Hello Rachit!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <hr />
+      <Test />
     </div>
   );
 }
